95da8187a47f88b90a67c690ea93af95c4b7da8b
vigneshckdev <vigneshckdev@gmail.com>
2025-10-17 23:58:09 +0530

    changes in the routes

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/post-job" element={<JobPost />} />
-      <Route path="/job/:id" element={<JobDetails/>}/>
+      <Route path="/jobInfo/:id" element={<JobDetails/>}/>
     </Routes>
   );
 }
